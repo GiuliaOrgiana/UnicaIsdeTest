62bfce5d22aaec4717005e3fea68167db60f30f1
g.orgiana <giuliaorgiana@gmail.com>
2021-10-23 16:03:33 +0200

    lez 08 10

--- src/main.py
+++ src/main.py
@@ -6,23 +6,30 @@ from nmc import NMC
 
 from sklearn.neighbors import NearestCentroid
 from sklearn.svm import SVC
-n_rep = 5
+from sklearn.model_selection import ShuffleSplit
+
+
 x, y = load_mnist_data()
 
-test_error = np.zeros(shape=(n_rep,))
-#clf = NMC()
-#clf = NearestCentroid()
-clf = SVC(C=100, kernel='linear')
+splitter = ShuffleSplit(n_splits=5, train_size=.5, random_state=658131)
 
-#clf.robust_estimation = True
-for r in range(n_rep):
-    print(r)
-    x_tr, y_tr, x_ts, y_ts = split_data(x, y, n_tr=1000)
+test_error = np.zeros(shape=(splitter.n_splits,))
+# clf = NMC()
+# clf = NearestCentroid()
+clf = SVC(C=1, kernel='linear')
+
+# clf.robust_estimation = True
+
+
+for i, (tr_idx, ts_idx) in enumerate(splitter.split(x, y)):
+    # x_tr, y_tr, x_ts, y_ts = split_data(x, y, n_tr=1000)
+    x_tr, y_tr = x[tr_idx, :], y[tr_idx]
+    x_ts, y_ts = x[ts_idx, :], y[ts_idx]
     clf.fit(x_tr, y_tr)
-    #plot_ten_digit(clf.centroids)
+    # plot_ten_digit(clf.centroids)
     ypred = clf.predict(x_ts)
-    test_error[r] = (ypred != y_ts).mean()
-
-#plot_ten_digit(clf.centroids)
+    test_error[i] = (ypred != y_ts).mean()
+    print(test_error)
+# plot_ten_digit(clf.centroids)
 
 print(test_error.mean(), test_error.std())
