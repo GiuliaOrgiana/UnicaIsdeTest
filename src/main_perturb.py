10b46b52da4aba72c22566f3a88d3a8f6cb7352b
g.orgiana <giuliaorgiana@gmail.com>
2021-10-24 14:32:34 +0200

    seconda parte lez 12/10

--- src/main_perturb.py
+++ src/main_perturb.py
@@ -7,10 +7,26 @@ from nmc import NMC
 from sklearn.neighbors import NearestCentroid
 from sklearn.svm import SVC
 
-from data_perturb import CDataPerturbRandom
+from data_perturb import CDataPerturbUniform, CDataPerturbGaussian
 
-def data_perturbation(x):
-    return x
+def robustness_test(clf, data_pert, param_name, param_values):
+
+    test_accuracies = np.zeros(shape=param_values.shape)
+    for i, k in enumerate(param_values):
+        # perturb ts
+        setattr(data_pert, param_name, k)
+        # data_pert.set(param_name, k)
+        data_pert.K = k
+        xp = data_pert.perturb_dataset(x_ts)
+        # plot_ten_digits(xp, y)
+        # compute predicted labels on the perturbed ts
+        y_pred = clf.predict(xp)
+        # compute classification accuracy using y_pred
+        clf_acc = np.mean(y_ts == y_pred)
+        print("test accuracy (K=:", k, "): ", int(clf_acc * 1000) / 10, "%")
+        test_accuracies[i] = clf_acc
+    return test_accuracies
+
 
 x, y = load_mnist_data()
 
@@ -21,42 +37,39 @@ x, y = load_mnist_data()
 # 4. we apply the data perturbation function
 # 5. we copy the result (so the perturbed samples) in Xp
 
-data_pert = CDataPerturbRandom()
-
 
 # plot_ten_digits(x, y)
 # plot_ten_digits(xp, y)
-xp = data_pert.perturb_dataset(x)
+
 
 # split MNITS data into 60% trainingset and 40% test set
 n_tr = int(0.6 * x.shape[0])
 print("Number of total samples: ", x.shape[0], "\nNumber of training samples: ", n_tr)
 x_tr, y_tr, x_ts, y_ts = split_data(x, y, n_tr=n_tr)
 
-clf = SVC(kernel='linear')
+clf = NMC()
 clf.fit(x_tr, y_tr)
 y_pred = clf.predict(x_ts)
 
 clf_acc = np.mean(y_ts == y_pred)
 print("test accuracy: ", int(clf_acc*1000)/10, "%")
 
-k_values = np.array([0, 10, 20, 50, 100, 200, 330, 400, 500])
-test_accuracies = np.zeros(shape=k_values.shape)
-for i,k in enumerate(k_values):
-    # perturb ts
-    data_pert.K = k
-    xp = data_pert.perturb_dataset(x_ts)
-    # plot_ten_digits(xp, y)
-    # compute predicted labels on the perturbed ts
-    y_pred = clf.predict(xp)
-    # compute classification accuracy using y_pred
-    clf_acc = np.mean(y_ts == y_pred)
-    print("test accuracy (K=:", k, "): ", int(clf_acc * 1000) / 10, "%")
-    test_accuracies[i] = clf_acc
+param_values = np.array([0, 10, 20, 50, 100, 200, 330, 400, 500])
+sigma_values = np.array([10, 20, 200, 300, 400])
 
 
-
-plt.plot(k_values, test_accuracies)
+plt.figure(figsize=(10,5))
+test_accuracies = robustness_test(
+    clf, CDataPerturbUniform(), param_name='K', param_values=param_values)
+plt.subplot(1,2,1)
+plt.plot(param_values, test_accuracies)
 plt.xlabel('K')
+plt.ylabel('Test accuracy(K)')
+
+test_accuracies = robustness_test(
+    clf, CDataPerturbGaussian(), param_name='sigma', param_values=param_values)
+plt.subplot(1,2,2)
+plt.plot(param_values, test_accuracies)
+plt.xlabel('sigma')
 plt.ylabel('Test accuracy(K)')
 plt.show()
